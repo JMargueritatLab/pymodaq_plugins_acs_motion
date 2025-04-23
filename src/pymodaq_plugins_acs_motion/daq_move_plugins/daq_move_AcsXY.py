dec96cf91140865953cf9ca5b8a74bfb2f8cf49f
Jeremie Margueritat <jeremie.margueritatuniv-lyon1.fr>
2025-04-23 21:01:40 +0200

    coorection of the local import

--- src/pymodaq_plugins_acs_motion/daq_move_plugins/daq_move_AcsXY.py
+++ src/pymodaq_plugins_acs_motion/daq_move_plugins/daq_move_AcsXY.py
@@ -6,8 +6,8 @@ from pymodaq.control_modules.move_utility_classes import (DAQ_Move_base, comon_p
 from pymodaq_utils.utils import ThreadCommand  # object used to send info back to the main thread
 from pymodaq_gui.parameter import Parameter
 
-from hardware.acscontrol import Controller  # ACS controller wrapper
-#from acspy.control import Controller
+from pymodaq_plugins_acs_motion.hardware.acscontrol import Controller  # ACS controller wrapper
+
 
 class DAQ_Move_AcsXY(DAQ_Move_base):
     """ Minimalistic plugin to control ACS motion stages with PyMoDAQ.
